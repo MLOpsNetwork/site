ee52e3e3dc7f838bae4f7b67180bb7cf404e4247
Gourav Shah <gs@initcron.org>
2025-02-11 12:29:05 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'MLOps Network',
-  tagline: 'Building AI's Future, One Pipeline at a Time',
+  tagline: 'Building Future of AI/ML, One Pipeline at a Time',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
